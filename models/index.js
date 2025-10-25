6e74374ff38bc26977b5a553533117da0378a0fe
Ifeoluwayemisi <destinifeoluwa@gmail.com>
2025-10-25 12:04:58 +0100

    specify module type

--- models/index.js
+++ models/index.js
@@ -5,15 +5,15 @@ import countryModel from "./country.js";
 dotenv.config();
 console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.PASS || process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 3306,
-        dialect: "mysql",
-        logging: false
-    }
+  process.env.MYSQL_ADDON_DB || process.env.DB_NAME,
+  process.env.MYSQL_ADDON_USER || process.env.DB_USER,
+  process.env.MYSQL_ADDON_PASSWORD || process.env.DB_PASS,
+  {
+    host: process.env.MYSQL_ADDON_HOST || process.env.DB_HOST,
+    port: process.env.MYSQL_ADDON_PORT || process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
 );
 
 const Country = countryModel(sequelize);
